0b12f8f2e64338bf5004a03963e9b2a69473677b
MrSentinel <82369913+Pseud0-space@users.noreply.github.com>
2023-10-29 08:46:52 +0530

    Add files via upload

---
+++ Palm.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import google.generativeai as palm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY=os.environ.get("PALM_API_KEY")
+palm.configure(api_key=API_KEY)
+
+def main():
+    st.image("./Google_PaLM_Logo.svg.webp", use_column_width=False, width=100)
+    st.header("Chat with PaLM")
+    st.write("")
+
+    prompt = st.text_input("Prompt please...", placeholder="Prompt", label_visibility="visible")
+    temp = st.slider("Temperature", 0.0, 1.0, step=0.05)
+
+    if st.button("SEND", use_container_width=True):
+        model = "models/text-bison-001"
+
+        response = palm.generate_text(
+            model=model,
+            prompt=prompt,
+            temperature=temp,
+            max_output_tokens=1024
+        )
+
+        st.write("")
+        st.header(":blue[Response]")
+        st.write("")
+
+        st.markdown(response.result, unsafe_allow_html=False, help=None)
+
+if __name__ == "__main__":
+    main()
